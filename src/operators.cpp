4b76219f80f3099478eee856ad2088862e09a249
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2023-11-07 01:57:48 +0300

    Support power operator

--- src/operators.cpp
+++ src/operators.cpp
@@ -7,6 +7,7 @@ const std::map<char, TArithmeticOperator> Operators::LIST = {
         { '*', { 3, std::multiplies<>{} } },
         { '/', { 3, std::divides<>{} } },
         { '%', { 3, [](double a, double b) { return (long)a % (long)b; } } },
+        { '^', { 3, [](double a, double b) { return pow(a, b); } } },
         { '~', { 4, [](double _, double x) { return -x; }, TArithmeticOperator::Type::UnaryPrefix } },
         { '!', { 4, [](double x, double _) {
             const long lim = (long)x;
